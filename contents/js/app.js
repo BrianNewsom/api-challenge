f31b180c7cc0d8c702fe836436f7585ec6d89a49
BrianNewsom <brian.newsom@colorado.edu>
2015-02-24 11:31:02 -0700

    Build in search bar - working, move buttons to jquery listeners

--- contents/js/app.js
+++ contents/js/app.js
@@ -1,8 +1,15 @@
-$( "#hotTracks" ).on( "click", function( event ) {
-    listTracks()
-})
 
-$( "#hotArtists" ).on( "click", function( event ) {
-    listArtists()
-})
+$(document).ready(function(){
+    $( "#hot-tracks" ).on( "click", function( event ) {
+        listTracks()
+    })
 
+    $( "#hot-artists" ).on( "click", function( event ) {
+        listArtists()
+    })
+
+    $('#submit-btn').on('click', function(event){
+        var query = $('#search-bar').val();
+        showTrack('' + query, null);
+    })
+})
