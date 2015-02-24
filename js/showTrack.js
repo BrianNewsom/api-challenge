4b324c70fba88dd34615f95092667bbf691d834f
BrianNewsom <brian.newsom@colorado.edu>
2015-02-24 12:07:54 -0700

    Build with search images

--- js/showTrack.js
+++ js/showTrack.js
@@ -6,8 +6,7 @@ function showTrack(unescaped_name, artist_id){
     var query = "http://developer.echonest.com/api/v4/artist/news?api_key=" + echonest.api_key + "&name=" + name + "&results=" + maxResults + "&start=0";
     $.get(query, function(data){
         var news = data.response.news;
-        console.log(news);
-        getPic(artist_id, function(err, picURL){
+        getPic(name,artist_id, function(err, picURL){
             $.get("/api-challenge/templates/trackView.jade", function(template){
                 var html = jade.render(template, {items: news, picURL: picURL, query: unescaped_name})
                 $("#details").html(html);
@@ -16,10 +15,26 @@ function showTrack(unescaped_name, artist_id){
     })
 }
 
-function getPic(artist_id, cb){
-    if (artist_id == null) {
-        cb('No pic', null);
+function getPic(name,artist_id, cb){
+    // Kill to break infinite loop
+    if (!name && !artist_id) {
+        cb('Pic not found', null);
+        console.log('No pic found');
         return;
+    }
+    // If we don't have id, get it
+    if (artist_id == null) {
+        var idQuery = 'http://developer.echonest.com/api/v4/artist/search?api_key=' + echonest.api_key + '&name=' + name
+        $.get(idQuery, function(idData){
+            if(idData.response.artists[0]) {
+                var new_artist_id = idData.response.artists[0].id;
+            } else {
+                var new_artist_id = null;
+            }
+                // Recurse with id
+                getPic(name, new_artist_id, cb);
+                return;
+        })
     } else {
         // Deferred objects
         var picQuery = 'http://developer.echonest.com/api/v4/artist/images?api_key=' + echonest.api_key + '&id=' + artist_id + '&format=json&results=1&start=0';
